849ffa7fd464d1d26a98709a16dd7ffa0a09ce46
Quentin Gérôme <quentin@letsbuildit.dev>
2024-11-26 21:39:40 +0100

    Initial Commit

---
+++ DisplayBLEDevices/DisplayBLEDevicesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisplayBLEDevicesApp.swift
+//  DisplayBLEDevices
+//
+//  Created by Quentin Gérôme on 26/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct DisplayBLEDevicesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DisplayBLEDevicesTests/DisplayBLEDevicesTests.swift
@@ -0,0 +1,17 @@
+//
+//  DisplayBLEDevicesTests.swift
+//  DisplayBLEDevicesTests
+//
+//  Created by Quentin Gérôme on 26/11/2024.
+//
+
+import Testing
+@testable import DisplayBLEDevices
+
+struct DisplayBLEDevicesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
